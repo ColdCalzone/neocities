a07c258ccd694d1cca5f111bff4f0cda9423bf10
ColdCalzone <45553542+ColdCalzone@users.noreply.github.com>
2024-02-10 18:47:54 -0500

    Add the projects page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
     import Footer from "$lib/Footer.svelte";
     import Navigation from "$lib/Navigation.svelte";
 </script>
+<head>
+    <title>Home of Cold Calzones</title>
+</head>
 
 <Navigation />
 

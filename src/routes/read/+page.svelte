9a9de5143d8c97f7a4ef49a14b55fc47dc7ae623
ColdCalzone <45553542+ColdCalzone@users.noreply.github.com>
2024-02-10 19:07:30 -0500

    Add Reading page

---
+++ src/routes/read/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    import Box from "$lib/Box.svelte";
+</script>
+
+<div id="container">
+    <div id="flex">
+        <main>
+            <h1>Reading List</h1>
+            <hr>
+            <Box>
+                <h1><a href="https://craftinginterpreters.com/">Crafting Interpreters</a></h1>
+                <hr>
+                <p>I got recommended this book by a friend when I asked about resources for making a compiler. Literally could not have asked for a better resource lmao. I must admit, though, that I wasn't able to fully follow the book, because I was using Rust, and found it difficult translating from Java. (It should be noted I was much worse at Rust at the time, though it would've been helpful to make another implementation first.)</p>
+            </Box>
+            <Box>
+                <h1><a href="https://homestuck.com/">Homestuck</a></h1>
+                <hr>
+                <p>My history with Homestuck is long, to say the least. Perhaps responsible for my interest in computer science being what it is today, and yet, paradoxically, my interest in Homestuck stems in my interest in programming (and Undertale...).</p>
+                <p>In recent times I've taken a much more active interest in Homestuck, to say the least. I have literally written essays on Homestuck, for fun -- you can read those <a href="/homestuck/">here</a>. I've also become involved in an <a href="https://acdsstuck.neocities.org">MSPFA</a> -- or "MS Paint Fan Adventure", based on the old title of Homestuck's website, mspaintadventures -- with 11 other people.</p>
+                <p>It starts slow, but it's worth it. About 8000 pages long.</p>
+                <p><a href="https://413.gay">Also, read it with the unnofficial collection, it's better.</a></p>
+            </Box>
+            <!-- <hr style="color:red"> -->
+            <!-- This line is meant to denote the line between things I have and have not read,
+                though for the life of me I cannot justify anything that would go below this line.
+                Despite what this page may imply, *I don't read much*, and if I wanted to read a book,
+                I'd just read it. Maybe if I start a real good book, I'll put it here before finishing it.
+                
+                Who knows. -->
+        </main>
+    </div>
+</div>

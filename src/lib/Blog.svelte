622dfb61258bd7aae8212a69297c67f35fc647cf
ColdCalzone <45553542+ColdCalzone@users.noreply.github.com>
2024-04-15 20:57:08 -0400

    Implement full-view of images in the blog | !UPD8

--- src/lib/Blog.svelte
+++ src/lib/Blog.svelte
@@ -4,6 +4,27 @@
     import { browser } from "$app/environment";
     import { onMount } from "svelte";
     
+    // Marked customization
+    const renderer = {
+        image(href : string, title : string, _text : string) {
+            return `
+                <!-- This is super jank -->
+                <a onclick="document.getElementById('fullview-${href}').style.display = 'block';">
+                    <img style="image-rendering: auto; max-width: 100%;" src="${href}" title="${title}">
+                </a>
+
+                <div id="fullview-${href}" style="position: fixed; top: 0; left: 0; width: 100vw; height: 100vh; background-color: #000000a0; display: none;">
+                    <a onclick="document.getElementById('fullview-${href}').style.display = 'none';" style="position: absolute; top: 1%; right: 1%;">X</a>
+                    <img src="${href}" title="${title}" style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); image-rendering: auto;">
+                </div>
+                `;
+        }
+    }
+
+    marked.use({ renderer });
+
+    // end of Marked customization
+    
     export let locked : string | undefined = undefined;
 
     let posts : string[] = [];

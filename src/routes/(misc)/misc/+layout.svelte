0f1247538f33def2ee75958d550de768370a582e
ColdCalzone <45553542+ColdCalzone@users.noreply.github.com>
2024-04-21 01:49:57 -0400

    shrink the top bar thing | !UPD8

--- src/routes/(misc)/misc/+layout.svelte
+++ src/routes/(misc)/misc/+layout.svelte
@@ -8,7 +8,7 @@
 </svelte:head>
 
 <main style="height: 100vh; background-color: #1b0320; display:flex; flex-direction: column;">
-  <div style="width:100%;height:30px; border: 2px solid #00ff00; border-radius: 10px;margin-bottom:2px;">
+  <div style="height:30px; border: 2px solid #00ff00; border-radius: 10px;margin-bottom:2px;">
     <a style="color: #00A639" href="/">BACK</a>
   </div>
 

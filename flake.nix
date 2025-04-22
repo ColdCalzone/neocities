09e884df2e7255e235f282bf44402d3400b9d51f
ColdCalzone <coldhotpocket1@gmail.com>
2025-04-22 14:22:59 -0400

    Rework flake.nix to be more central to development

--- flake.nix
+++ flake.nix
@@ -1,4 +1,7 @@
 {
+
+  description = "";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -7,17 +10,31 @@
     flake-utils.lib.eachDefaultSystem
       (system:
         let
-          overlays = [ ];
-          pkgs = import nixpkgs {
-            inherit system overlays;
-          };
+          pkgs = import nixpkgs { inherit system; };
+          nativeBuildInputs = with pkgs; [
+            wasm-pack
+            nodejs
+          ];
         in
         with pkgs;
         {
-          devShells.default = mkShell {
-            buildInputs = [
-              wasm-pack
-            ];
+          packages.default = buildNpmPackage {
+            pname = "deep-freezer";
+            version = "1.0.0";
+
+            nativeBuildInputs = nativeBuildInputs;
+
+            src = ./.;
+
+            npmDepsHash = "sha256-aX5sIAfW+9d7pPg9FBjiiSGNjbKE2KoE97tKtKMXDKA=";
+
+            npmBuild = ''
+              npm run build
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp -r public/* $out
+            '';
           };
         }
       );
